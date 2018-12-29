b2b9d5fe4a6aa0e5e1bdd92ee82385ebfca57c42
Christoph Schorn <christoph.schorn1@rwth-aachen.de>
2018-12-29 02:17:21 +0100

    Modify JS, add custom parts, bundle all files into one with jekyll/yaml front matter

---
+++ js/bundle.js
@@ -0,0 +1,13 @@
+---
+layout: 
+---
+
+{% include_relative plugins/history.js %} 
+{% include_relative plugins/imagesloaded.js %} 
+{% include_relative plugins/masonry.js %} 
+{% include_relative plugins/debounce.js %} 
+{% include_relative plugins/fluidbox.js %} 
+{% include_relative plugins/owl.js %} 
+{% include_relative plugins/waypoints.js %} 
+
+{% include_relative simples.js %}
